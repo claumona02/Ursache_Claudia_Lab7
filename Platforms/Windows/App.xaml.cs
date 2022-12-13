da4078caf49a986c318d4b1bfa2f521622aa3c92
claumona02@yahoo.com <claumona02@yahoo.com>
2022-12-13 18:42:50 +0200

    laborator9

--- Platforms/Windows/App.xaml.cs
+++ Platforms/Windows/App.xaml.cs
@@ -3,6 +3,7 @@
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
+
 namespace Ursache_Claudia_Lab7.WinUI;
 
 /// <summary>
@@ -10,14 +11,16 @@ namespace Ursache_Claudia_Lab7.WinUI;
 /// </summary>
 public partial class App : MauiWinUIApplication
 {
-	/// <summary>
-	/// Initializes the singleton application object.  This is the first line of authored code
-	/// executed, and as such is the logical equivalent of main() or WinMain().
-	/// </summary>
-	public App()
+    /// <summary>
+    /// Initializes the singleton application object.  This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// </summary>
+  
+
+    public App()
 	{
 		this.InitializeComponent();
-	}
+    }
 
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
